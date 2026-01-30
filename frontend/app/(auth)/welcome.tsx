92eb2639cda618b0dcf2bd41b3e78d43a2783a54
munirhankynk <munirhankynk@gmail.com>
2026-01-30 21:51:59 +0300

    Add screem wrapper component

--- frontend/app/(auth)/welcome.tsx
+++ frontend/app/(auth)/welcome.tsx
@@ -1,11 +1,12 @@
+import ScreenWrapper from '@/components/ScreenWrapper';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 
 const Welcome = () => {
     return (
-        <View>
+        <ScreenWrapper showPattern={true}>
             <Text>Welcome Screen</Text>
-        </View>
+        </ScreenWrapper>
     );
 };
 
