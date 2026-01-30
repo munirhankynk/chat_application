0a8e99fa52a0c6a1a38dc16930c23ce6148f0d06
munirhankynk <munirhankynk@gmail.com>
2026-01-30 17:42:00 +0300

    Add app opening and logo

---
+++ frontend/app/(auth)/welcome.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Welcome = () => {
+    return (
+        <View>
+            <Text>Welcome Screen</Text>
+        </View>
+    );
+};
+
+export default Welcome;
+const styles = StyleSheet.create({});
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,34 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
+import { colors } from "@/constants/theme";
+import { useRouter } from "expo-router";
+import React, { useEffect } from "react";
+import { StatusBar, StyleSheet, View } from "react-native";
+import Animated, { FadeInDown } from "react-native-reanimated";
 
-export default function Page() {
+const SplashScreen = () => {
+
+  const router = useRouter()
+  useEffect(() => { 
+    setTimeout(() => {
+      router.replace("/(auth)/welcome")
+      
+    }, 1500);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
+        <StatusBar 
+        barStyle={'light-content'} 
+        backgroundColor={colors.neutral900} />
+
+        <Animated.Image
+            source={require("../assets/images/splashImage.png")}
+            entering={FadeInDown.duration(700).springify()}
+            style={styles.logo}
+            resizeMode={"contain"}
+        />
     </View>
   );
-}
+};
+
+export default SplashScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
+    container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor: colors.neutral900
+    },
+    logo:{
+        height: "23%",
+        aspectRatio: 1,
+    },
 });
