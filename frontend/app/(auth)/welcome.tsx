5e2174363abc379cddf50923a01ed2e490639562
munirhankynk <munirhankynk@gmail.com>
2026-02-01 15:40:26 +0300

    Add typo and button component

--- frontend/app/(auth)/welcome.tsx
+++ frontend/app/(auth)/welcome.tsx
@@ -1,14 +1,62 @@
+import Button from '@/components/Button';
 import ScreenWrapper from '@/components/ScreenWrapper';
-import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import Typo from '@/components/Typo';
+import { colors, spacingX, spacingY } from '@/constants/theme';
+import { verticalScale } from '@/utils/styling';
+import { StyleSheet } from 'react-native';
+import Animated, { FadeIn } from 'react-native-reanimated';
+import { View } from 'react-native-reanimated/lib/typescript/Animated';
 
 const Welcome = () => {
     return (
         <ScreenWrapper showPattern={true}>
-            <Text>Welcome Screen</Text>
+            <View style={styles.container}>
+                <View style={{alignItems: 'center'}}>
+                    <Typo color={colors.white} size={43} fontWeight={"900"}>
+                        LeBron
+                    </Typo>
+
+                </View>
+                <Animated.Image
+                    entering={FadeIn.duration(700).springify()}
+                    source={require("../../assets/images/welcomeImage.png")}
+                    style={styles.welcomeImage}
+                    resizeMode={"contain"}
+                />
+
+                <View>
+                    <Typo color={colors.white} size={33} fontWeight={"800"}>
+                        Stay Connected
+                    </Typo>
+                    <Typo color={colors.white} size={33} fontWeight={"800"}>
+                        with your friends
+                    </Typo>
+                    <Typo color={colors.white} size={33} fontWeight={"800"}>
+                        and family.
+                    </Typo>
+                </View>
+                <Button/>
+            </View>
         </ScreenWrapper>
     );
 };
 
 export default Welcome;
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "space-around",
+        paddingHorizontal: spacingX._20,
+        marginVertical: spacingY._10,
+    },
+    background: {
+        flex: 1,
+        backgroundColor: colors.neutral900,
+    },
+    welcomeImage: {
+        height: verticalScale(300),
+        aspectRatio: 1,
+        alignSelf: "center",
+    },
+});
+    
