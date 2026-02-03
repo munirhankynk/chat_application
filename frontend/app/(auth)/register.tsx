11ab3d4d2bf886d8b79c1a29bf38a9ca29f84501
munirhankynk <munirhankynk@gmail.com>
2026-02-03 03:31:28 +0300

    Add BackButton component and update register page

--- frontend/app/(auth)/register.tsx
+++ frontend/app/(auth)/register.tsx
@@ -1,14 +1,46 @@
+import BackButton from "@/components/BackButton";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
-import { colors, spacingX, spacingY } from "@/constants/theme";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
 import React from "react";
-import { StyleSheet } from "react-native";
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, View } from "react-native";
+
 
 const Register = () => {
   return (
-    <ScreenWrapper>
-      <Typo color={colors.white}>Register Screen</Typo>
+    <KeyboardAvoidingView 
+    style={{flex: 1}} 
+    behavior={Platform.OS=='ios' ? 'padding' : 'height'}
+    >
+    <ScreenWrapper showPattern={true}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+            <BackButton iconSize={28} />
+            <Typo size={17} color={colors.white}>
+                Need help?
+            </Typo>
+        </View>
+
+        <View style={styles.content}>
+            <ScrollView 
+            contentContainerStyle={styles.form}
+            showsVerticalScrollIndicator={false}
+            >
+                <View style={{gap: spacingY._10, marginBottom: spacingY._15}}>
+                    <Typo size={28} fontWeight='600'>
+                        Getting Started
+                    </Typo>
+                    <Typo color={colors.neutral600}>
+                        Create an account to continue
+                    </Typo>
+                </View>
+
+                
+            </ScrollView>
+        </View>
+      </View>
     </ScreenWrapper>
+    </KeyboardAvoidingView>
   );
 }
 
